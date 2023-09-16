4ef1e68ebde314797f1239c5bf57c0e686083f4b
Flying-NocchiMan <rgz91rx93paris.0214@gmail.com>
2023-09-16 14:49:41 +0900

    JavaScript課題リポジトリ作成

---
+++ javascript_basic_kadai/kadai_005/bmi.js
@@ -0,0 +1,19 @@
+// 体重と身長のBMI（体格指数）の値は、変数
+// 正しいBMI（体格指数）の計算結果が表示されている
+// 体重：68kg
+// 身長：1.7m
+// 計算式：[体重(kg)]÷[身長(m)×身長(m)]
+
+// 変数の宣言 値の代入
+let weightKg = 68; 
+let heightM = 1.7;
+
+// BMIを計算
+let Bmi = weightKg / (heightM * heightM);
+
+// コンソールへの出力
+// console.log(weightKg);
+// console.log(heightM);
+console.log(Bmi);
+
+
