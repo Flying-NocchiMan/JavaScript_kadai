97f4e287fe34bb4f16695c1173782fcb6d256bcb
Flying-NocchiMan <rgz91rx93paris.0214@gmail.com>
2023-09-20 20:52:17 +0900

    課題13を新規追加

---
+++ javascript_basic_kadai/kadai_013/square.js
@@ -0,0 +1,8 @@
+function squareAndPrintResult(number) {
+  const squared = number * number;
+  console.log("計算結果:", squared);
+}
+
+// 関数を呼び出すために、以下の行を追加します。
+squareAndPrintResult(5); // この行で関数を呼び出します。
+
