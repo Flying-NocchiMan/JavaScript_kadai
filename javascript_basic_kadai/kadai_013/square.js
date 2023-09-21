b2d474e2791eab8063b6f4fbc724658624960fbe
Flying-NocchiMan <rgz91rx93paris.0214@gmail.com>
2023-09-21 21:45:45 +0900

    課題13修正

--- javascript_basic_kadai/kadai_013/square.js
+++ javascript_basic_kadai/kadai_013/square.js
@@ -1,8 +1,18 @@
-function squareAndPrintResult(number) {
+// function squareAndPrintResult(number) {
+//   const squared = number * number;
+//   // console.log("計算結果:", squared);
+// }
+
+// // 関数を呼び出すために、以下の行を追加します。
+// squareAndPrintResult(5); // この行で関数を呼び出します。
+// console.log(squared());
+
+
+function squareNumber(number) {
   const squared = number * number;
-  console.log("計算結果:", squared);
+  return squared; // 二乗した結果を戻り値として返す
 }
 
-// 関数を呼び出すために、以下の行を追加します。
-squareAndPrintResult(5); // この行で関数を呼び出します。
-
+// 関数を呼び出し、計算結果をコンソールに出力
+const result = squareNumber(5);
+console.log("計算結果:", result);
